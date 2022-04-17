159fd6a6ae5f46004a8d5f54df55e7008ddad38c
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2022-04-17 23:48:28 +0530

    add login form validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,45 @@
 var authService = require('../services/authService');
+const Joi = require('joi');
 
+// Show login form
 exports.auth_login = async function (req, res) {
   res.render('login');
 };
 
-exports.handle_login = async function (req, res) {
-  var data = req.body;
-  var user = await authService.validate(data.username, data.password);
+// Validate login data
+exports.validate_login_data = async function (req, res, next) {
+  
+  const schema = Joi.object({
+    username: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
 
+  try {
+    req.validatedData = await schema.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true,
+    });
+    next();
+  } catch (err) {
+    var errorMessages = err.details.map((el) => el.message);
+    req.session.messages = errorMessages;
+    req.session.save(function (err) {
+      return res.redirect('/login');
+    });
+  }
+};
+
+// Handle user login
+exports.handle_login = async function (req, res, next) {
+  const validatedData = req.validatedData;
+
+  user = await authService.validate(
+    validatedData.username,
+    validatedData.password,
+  );
+
+  // Update session if creds are valid
+  // Othwerise throw error
   if (user) {
     req.session.auth = JSON.stringify(user);
     req.session.save(function (err) {
@@ -25,6 +57,7 @@ exports.handle_login = async function (req, res) {
   }
 };
 
+// Logout user
 exports.auth_logout = async function (req, res) {
   req.user = null;
   req.session.auth = null;
--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,14 @@ const {
   auth_login,
   auth_logout,
   handle_login,
+  validate_login_data,
 } = require('../controllers/authController');
 const ensureIsAuthenticated = require('../middlewares/ensureIsAuthenticated');
 
 var router = express.Router();
 
 router.get('/login', ensureIsUnauthenticated, auth_login);
-router.post('/login', ensureIsUnauthenticated, handle_login);
+router.post('/login', ensureIsUnauthenticated, validate_login_data, handle_login);
 router.post('/logout', ensureIsAuthenticated, auth_logout);
 
 module.exports = router;
